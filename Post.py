7bf5cbbb853ce09235ff160c1d94ec161b0bce75
Arnon Puitrakul <peter_arnon1@icloud.com>
2020-04-25 13:34:25 +0700

    🎉 Init Commit

---
+++ Post.py
@@ -0,0 +1,125 @@
+import json
+import datetime
+
+class Post: 
+    def __init__(self, id=1, title="title", slug="slug", markdown="content", excerpt="excerpt"):
+        super().__init__()
+        self.id = id
+        self.title = title
+        self.slug = slug
+        self.markdown = markdown
+        self.excerpt = excerpt
+        self.tags = []
+
+    def add_tag (self, new_tag_name) :
+        if new_tag_name not in self.tags :
+            self.tags.append(new_tag_name)
+
+    def convert_tag_to_id (self, tag_dict) :
+        new_tags = []
+
+        for tag in self.tags :
+            new_tags.append(tag_dict[tag])
+        self.tags = new_tags
+    
+    def convert_feature_image_path (self, image_dict) :
+        if getattr(self, 'feature_image', False) :
+            self.feature_image = image_dict[self.feature_image]
+
+    def assign_id (self, id) :
+        self.id = id
+
+    def serialise (self) :
+        post_dump = {
+            "id":            self.id,
+            "title":         self.title,
+            "slug":          self.slug,
+            "mobiledoc" :    json.dumps({
+                "version" : "0.3.1",
+                "atoms" : [],
+                "cards" : [],
+                "markups" : [],
+                "sections" : []
+            }),
+            "feature_image": getattr(self, 'feature_image', None),
+            "featured":      getattr(self, 'featured', 0), 
+            "page":          getattr(self, 'is_page', 0),
+            "status":        getattr(self, 'is_published', "published"),
+            "published_at":  getattr(self, 'publish_date', datetime.datetime.now().timestamp()),
+            "published_by":  getattr(self, 'author', 1),
+            "meta_title":    self.title,
+            "meta_description":getattr(self, 'excerpt', None),
+            "author_id":     getattr(self, 'author', 1),
+            "created_at":    getattr(self, 'publish_date', datetime.datetime.now().timestamp()),
+            "created_by":    getattr(self, 'author', 1),
+            "updated_at":    getattr(self, 'publish_date', datetime.datetime.now().timestamp()),
+            "updated_by":    getattr(self, 'author', 1),
+            "custom_excerpt":getattr(self, 'excerpt', None),
+            "markdown": self.markdown, 
+        }
+
+        return post_dump
+
+class MDF :
+    def __init__(self, file_path):
+        self.file_pointer = open(file_path, 'r')
+
+        file_components = self.file_pointer.read().split('---')[1:]
+        
+        content = file_components[1]
+
+        self.post = Post()
+
+        # Trim line breaking on the first char of content
+        if content[0] == "\n" :
+            content = content[1:]
+
+        self.post.markdown = content
+
+        # Metadata Decomposition
+        metadata_list = file_components[0].split("\n")
+
+        for metadata in metadata_list :
+            decomposed_meta = metadata.split(':')
+            
+            if len(decomposed_meta) < 2 :
+                continue
+
+            decomposed_meta[0] = decomposed_meta[0].replace(' ', '')
+
+            if decomposed_meta[0] == 'title' :
+                self.post.title = decomposed_meta[1].replace('"', '')
+            elif decomposed_meta[0] == 'image' and len(decomposed_meta[1].replace(' ', '')) > 0:
+                if len(decomposed_meta[1].split('/')) > 1 :
+                    feature_image_name = decomposed_meta[1].split('/')[-1]
+                else :
+                    feature_image_name = decomposed_meta[1]
+
+                self.post.feature_image = feature_image_name.replace('"', '').replace('./', '').replace(' ' ,'')
+            
+            elif decomposed_meta[0] == 'category' :
+                category_name = decomposed_meta[1].replace('"', '')
+                if category_name[0] == " " :
+                    category_name = category_name[1:]
+                
+                if category_name[-1] == " " :
+                    category_name = category_name[:-1]
+                    
+                self.post.add_tag(category_name)
+            
+            elif decomposed_meta[0] == 'excerpt' :
+                self.post.excerpt = decomposed_meta[1].replace('"', '')
+            
+            elif decomposed_meta[0] == 'date' :
+                self.post.excerpt = int(datetime.datetime.strptime(decomposed_meta[1].replace(' ', '').replace('"', '').split('T')[0], '%Y-%m-%d').timestamp())
+            
+            elif decomposed_meta[0] == 'is_page' :
+                self.post.page = 0 if decomposed_meta[1] == "post" else 1
+            
+            elif decomposed_meta[0] == 'featured' :
+                self.post.featured = 0 if "false" in decomposed_meta else 1
+            
+            elif decomposed_meta[0] == 'status' :
+                self.post.is_published = decomposed_meta[1].replace(' ', '')
+
+        self.file_pointer.close()
---
+++ main.py
@@ -0,0 +1,177 @@
+from shutil import copy2
+
+import os
+import sys
+import datetime
+import json
+
+from Post import MDF
+
+def preparing_file_list (base_path, content_list) :
+    image_files_path = []
+    markdown_file_path = []
+
+    for content in content_list :
+        if not os.path.isdir(os.path.join(base_path,content)):
+            continue
+
+        file_list = os.listdir(os.path.join(base_path, content))
+        for filename in file_list :
+            # Check for file extension
+            file_parts = filename.split('.')
+            final_file_path = os.path.join(base_path, content, filename)
+            
+            if len(file_parts) > 1 :
+                # Markdown Extension
+                if file_parts[-1] == "md" :
+                    markdown_file_path.append(final_file_path)
+                # Other categorise to image
+                else :
+                    image_files_path.append(final_file_path)
+    
+    return markdown_file_path, image_files_path           
+
+def write_json_to_destination (destination_path, posts, post_tag, tags) :
+    dump_posts = []
+
+    for post in posts :
+        dump_posts.append(post.serialise())
+
+    json_dump = {
+        "db" :[
+            {
+                "meta" :
+                {
+                    "exported_on" : datetime.datetime.now().timestamp(),
+                    "version" : "3.13.4"
+                },
+                "data" :
+                {
+                    "posts" : dump_posts,
+                    "tags" : tags,
+                    "posts_tags" : post_tag
+                }
+
+            }
+        ]
+    }
+
+    destination_file = open(os.path.join(destination_path, 'data.json'), 'w')
+    destination_file.write(json.dumps(json_dump))
+    destination_file.close()
+
+    return json_dump
+# Image Helper Functions
+def configurating_image_path (destination_path) :
+    # Create base folder for images
+    if not os.path.exists(os.path.join(destination_path, 'content')) :
+        os.makedirs(os.path.join(destination_path, 'content'))
+
+def convert_to_valid_month_format (input_month) :
+    if len(str(input_month)) == 1 :
+        return "0" + str(input_month)
+    else :
+        return str(input_month)
+
+def create_image_path_convertor (image_file_path, destination_path) :
+    # Convert from Original to New Path
+    image_path_convertor = dict()
+
+    for image_file in image_file_path :
+        file_create_datetime = datetime.datetime.fromtimestamp(os.stat(image_file).st_ctime)        
+        new_path = os.path.join(destination_path, 'content', str(file_create_datetime.year), convert_to_valid_month_format(file_create_datetime.month), image_file.split('/')[-1])
+        image_path_convertor[image_file] = new_path
+
+    return image_path_convertor
+
+def create_image_file_name_to_path (image_path_convertor) :
+    image_file_name_to_path = dict()
+
+    for new_path in image_path_convertor.values() :
+        image_file_name_to_path[new_path.split('/')[-1]] = new_path
+    return image_file_name_to_path
+
+def image_mover (image_path_convertor) :
+    for source in image_path_convertor.keys() :
+        os.makedirs(os.path.dirname(image_path_convertor[source]), exist_ok=True)
+        copy2(source, image_path_convertor[source])
+
+# Markdown Helper Function
+def process_markdown (markdown_file_path_list, image_path_convertor) :
+    posts = []
+    tag_cloud = {}
+
+    # Tag Cloud slug -> {name: tag_name, asso_posts: [1,2,3,4]}}
+
+    for MD_path in markdown_file_path_list :
+        current_post = MDF(MD_path).post
+        current_post.convert_feature_image_path(image_path_convertor)
+        current_post.assign_id(len(posts)+1)
+
+        # Create Non-Exist Tag
+        for tag in current_post.tags :
+            tag_slug = tag.replace(' ' , '-').lower()
+
+            # Not Exist -> Create New One
+            if tag_slug not in tag_cloud :
+                tag_cloud[tag_slug] = {
+                    "id" : len(tag_cloud)+1,
+                    "name" : tag,
+                    "asso_posts" : []
+                }
+            
+            # Save in asso_posts
+            tag_cloud[tag_slug]['asso_posts'].append(current_post.id)
+        posts.append(current_post)
+    
+    return posts,tag_cloud
+    
+# Tag Helper Function
+def get_tags (tag_cloud) :
+    final_tags = []
+
+    for slug in tag_cloud.keys() :
+        current_tag = {
+            "id" : tag_cloud[slug]['id'],
+            "name": tag_cloud[slug]['name'],
+            "slug": slug
+        }
+        final_tags.append(current_tag)
+    return final_tags
+
+def get_post_tag (tag_cloud) :
+    final_post_tag = []
+
+    for slug in tag_cloud.keys() :
+        current_tag = tag_cloud[slug]
+
+        for post_id in current_tag['asso_posts']:
+            final_post_tag.append({
+                "id": len(final_post_tag)+1,
+                "post_id": post_id,
+                "tag_id" : current_tag['id']
+            })
+
+    return final_post_tag
+def main (args) :
+    base_path = args[1]
+    destination_path = args[2]
+
+    configurating_image_path(destination_path)
+    content_list = os.listdir(base_path)
+    markdown_file_path, image_file_path = preparing_file_list(base_path, content_list)
+
+    print("There are " + str(len(content_list)) + " contents , " + str(len(markdown_file_path)) + " Markdown(s) with " + str(len(image_file_path)) + " image(s) to be converted")
+
+    # Image Management
+    image_path_convertor = create_image_path_convertor(image_file_path, destination_path)
+
+    # image_mover(image_path_convertor)
+    # print(str(len(image_path_convertor)) + " files has been moved.")
+
+    # Markdown File Management
+    posts, tag_cloud = process_markdown(markdown_file_path, create_image_file_name_to_path(image_path_convertor))
+    write_json_to_destination (destination_path, posts, get_post_tag(tag_cloud), get_tags(tag_cloud))
+    
+if __name__ == "__main__":
+    main(sys.argv)
